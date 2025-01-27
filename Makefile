708703ae5b8e1a2e29cec7b45bf5da9aac11336e
Leo C. Stein <leo.stein@gmail.com>
2025-01-27 15:17:25 -0600

    Specify which fields to fetch from INSPIRE, vastly reducing JSON

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ index.html: lrr.json preamble.txt superseded.txt corrections.json lrr-index.py
 	./lrr-index.py --file $< > $@
 
 lrr.json:
-	curl -o $@ "https://inspirehep.net/api/literature?q=find+j+%22Living+Rev.Rel.%22&format=json&size=1000"
+	curl -o $@ 'https://inspirehep.net/api/literature?q=find+j+%22Living+Rev.Rel.%22&format=json&size=1000&fields=authors.full_name,authors.last_name,first_author,abstracts,titles,dois,publication_info.journal_title,publication_info.journal_volume,publication_info.year,publication_info.artid,publication_info.page_start,arxiv_eprints'
 
 lrr.xml:
 	curl -o $@ "http://old.inspirehep.net/search?p=find+j+%22Living+Rev.Rel.%22&of=xe&rg=1000"
--- lrr-index.py
+++ lrr-index.py
@@ -246,7 +246,13 @@ def stringFromURL(url):
 defaultFilename = 'lrr.json'
 defaultURL      = ('https://inspirehep.net/api/literature'
                    '?q=find+j+%22Living+Rev.Rel.%22'
-                   '&format=json&size=1000')
+                   '&format=json&size=1000'
+                   '&fields=authors.full_name,authors.last_name,'
+                   'first_author,abstracts,titles,dois,'
+                   'publication_info.journal_title,'
+                   'publication_info.journal_volume,publication_info.year,'
+                   'publication_info.artid,publication_info.page_start,'
+                   'arxiv_eprints')
 supersededFilename = 'superseded.txt'
 preambleFilename = 'preamble.txt'
 correctionsFilename = 'corrections.json'
